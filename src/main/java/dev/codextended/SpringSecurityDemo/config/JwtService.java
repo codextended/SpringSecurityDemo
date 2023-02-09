8abc3e7efad8f9a63e402a276d8f8a4287516983
Smath Cadet <thesmartone0@gmail.com>
2023-02-09 07:59:19 -0500

    added claims

---
+++ src/main/java/dev/codextended/SpringSecurityDemo/config/JwtService.java
@@ -0,0 +1,39 @@
+package dev.codextended.SpringSecurityDemo.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "8557BDD8363B52469575441AAF441";
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractALlClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractALlClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
